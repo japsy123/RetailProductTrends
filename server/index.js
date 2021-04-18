ea53558da695682ba9804c72509e29eaebad637d
Jal Panchal Panchal (DREVOL LLC) <v-japanc@microsoft.com>
2021-04-18 10:49:31 -0700

    Update server

--- server/index.js
+++ server/index.js
@@ -9,17 +9,16 @@ app.use(express.json());
 
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-
+    app.get("/product", (req, res) => {
+      res.json(productData);
+    });
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-};
 
-app.get("/product", (req, res) => {
-  res.json(productData);
-});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
