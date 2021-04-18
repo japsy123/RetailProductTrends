b67ecc73f08d215decfe9db6c4f2db38964b0f48
Jal Panchal Panchal (DREVOL LLC) <v-japanc@microsoft.com>
2021-04-18 10:55:07 -0700

    Remove cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const productData = require("./data");
 const express = require("express");
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 
-app.use(cors());
 
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get("/product", (req, res) => {
