2154625063d7e1024a4cfcb9b405de0ac1553962
Jal Panchal Panchal (DREVOL LLC) <v-japanc@microsoft.com>
2021-04-18 19:44:44 -0700

    Updates for heroku

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
 const productData = require("./data");
 const express = require("express");
+const cors = require("cors")
+var path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get("/product", (req, res) => {
+    res.json(productData);
+});
 
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get("/product", (req, res) => {
-      res.json(productData);
-    });
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-
-
-
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
